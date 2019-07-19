e78459c3b1067300aa44d30a829bc394f6111728
Nacho Alonso <narchercher@gmail.com>
2019-07-19 11:29:54 +0200

    Basket adjustment

--- basket.js
+++ basket.js
@@ -9,8 +9,8 @@ class Basket {
 		this.velY = 1
 		this.width = 15
 		this.x = x
-		this.y = this.gameHeight / 2 - this.height
-		this.y0 = this.gameHeight / 2 - this.height
+		this.y = this.gameHeight - 350 - this.height
+		this.y0 = this.gameHeight - 350 - this.height
 		this.rimHeight = 10
 		this.rimWidth = 80
 		this.rimX = (this.x - this.rimWidth + this.distance) * this.direction
@@ -32,6 +32,6 @@ class Basket {
 	move() {
 		this.y -= this.velY
 		this.rimY -= this.velY
-		if (this.y < 0 || this.y > this.y0) this.velY *= -1
+		if (this.y < this.y0 - 120 || this.y > this.y0) this.velY *= -1
 	}
 }
